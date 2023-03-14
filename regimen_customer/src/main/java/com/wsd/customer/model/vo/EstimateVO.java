3719769cd35b6dceea497f83bfcde1df69b800e4
wu songda <986476925@qq.com>
2023-03-14 16:07:20 +0800

    1.添加 评估记录响应实体注释

--- regimen_customer/src/main/java/com/wsd/customer/model/vo/EstimateVO.java
+++ regimen_customer/src/main/java/com/wsd/customer/model/vo/EstimateVO.java
@@ -14,32 +14,39 @@ import lombok.Data;
 @Schema(description = "评估记录信息")
 public class EstimateVO {
 
+    @Schema(description = "评估记录id", example = "1")
     private Long id;
     /**
      * 用户名称
      */
+    @Schema(description = "用户名称", example = "12")
     private String customerName;
 
     /**
      * 性别
      */
+    @Schema(description = "性别", example = "1")
     private Integer sex;
 
     /**
      * 年龄
      */
+    @Schema(description = "年龄", example = "1")
     private Integer age;
     /**
      * 商品id
      */
+    @Schema(description = "商品id", example = "1")
     private Long commodityId;
 
     /**
      * 商品名称
      */
+    @Schema(description = "商品名称", example = "测试1")
     private String commodityName;
     /**
      * 商品介绍
      */
+    @Schema(description = "商品介绍", example = "好好好")
     private String commodityIntroduce;
 }
